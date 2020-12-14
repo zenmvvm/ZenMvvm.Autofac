d779f18540c8228fbfb34f9ab7bf43d71e7e9c71
Zeebs <zeebsmail@gmail.com>
2020-12-14 11:28:29 +0200

    Replace SmartDi with Autofac

--- src/ZenMvvm.Autofac.Tests/IocTests.cs
+++ src/ZenMvvm.Autofac.Tests/IocTests.cs
@@ -1,88 +1,44 @@
-﻿using System;
-using Moq;
-using SmartDi;
+﻿using Autofac;
 using Xunit;
 
+// Note - Autofac uses sealed classes which make mocking with Moq impossible
+// therefore tests are also testing Autofac code
 namespace ZenMvvm.Autofac.Tests
 {
+
     public class IocTests
     {
         [Fact]
         public void Init_SetsContainer()
         {
             Ioc.Container = null;
-
-            Ioc.Init();
+            Ioc.Init(new ContainerBuilder());
 
             Assert.NotNull(Ioc.Container);
         }
+
         [Fact]
         public void Init_SetsViewModelLocatorIoc()
         {
             Ioc.Container = null;
             ViewModelLocator.Ioc = null;
 
-            Ioc.Init();
+            Ioc.Init(new ContainerBuilder());
 
             Assert.NotNull(ViewModelLocator.Ioc);
         }
 
-        [Fact]
-        public void Init_settings_SetsContainer()
-        {
-            Ioc.Container = null;
-            var mockSettings = new Mock<ContainerOptions>();
-
-            Ioc.Init(mockSettings.Object);
-
-            Assert.NotNull(Ioc.Container);
-        }
-
-        [Fact]
-        public void Init_settings_SetsViewModelLocatorIoc()
-        {
-            Ioc.Container = null;
-            ViewModelLocator.Ioc = null;
-            var mockSettings = new Mock<ContainerOptions>();
-
-            Ioc.Init(mockSettings.Object);
-
-            Assert.NotNull(ViewModelLocator.Ioc);
-        }
-
-        [Fact]
-        public void Init_configureSettings_SetsContainer()
-        {
-            Ioc.Container = null;
-            var mockSettingsAction = new Mock<Action<ContainerOptions>>();
-
-            Ioc.Init(mockSettingsAction.Object);
-
-            Assert.NotNull(Ioc.Container);
-        }
-
-        [Fact]
-        public void Init_configureSettings_SetsViewModelLocatorIoc()
-        {
-            Ioc.Container = null;
-            ViewModelLocator.Ioc = null;
-            var mockSettingsAction = new Mock<Action<ContainerOptions>>();
-
-            Ioc.Init(mockSettingsAction.Object);
-
-            Assert.NotNull(ViewModelLocator.Ioc);
-        }
 
         [Fact]
         public void Init_Registers_INavigationService()
         {
             Ioc.Container = null;
             ViewModelLocator.Ioc = null;
-            var mockContainer = new Mock<IDiContainer>();
+            var builder = new ContainerBuilder();
 
-            Ioc.Init(mockContainer.Object);
+            Ioc.Init(builder);
 
-            mockContainer.Verify(c => c.Register<INavigationService, NavigationService>(Type.EmptyTypes));
+            Assert.IsType<NavigationService>(Ioc.Container.Resolve(typeof(INavigationService)));
         }
     }
 }
